eebe6516947ed4f2aa0be1886253465e9f42b3eb
kjminn <kjminn2001@gmail.com>
2021-07-28 14:15:05 +0900

    edit MainActivity.java(직접풀어보기12_2) commit

--- app/src/main/java/kr/hs/emirim/kjminn/emirim_project12_2/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/kjminn/emirim_project12_2/MainActivity.java
@@ -29,6 +29,28 @@ public class MainActivity extends AppCompatActivity {
         Button btnInit = findViewById(R.id.btn_init);
         Button btnInput = findViewById(R.id.btn_input);
         Button btnSearch = findViewById(R.id.btn_search);
+        Button btnUpdate = findViewById(R.id.btn_update);
+        Button btnDelete = findViewById(R.id.btn_delete);
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db = dbHelper.getWritableDatabase();
+                db.execSQL("update groupTB set count = "+editCount.getText().toString()+ " where name='"+editName.getText().toString()+"';");
+                selectDB();
+                db.close();
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db = dbHelper.getWritableDatabase();
+                db.execSQL("delete from groupTB where name='"+editName.getText().toString()+"';");
+                selectDB();
+                db.close();
+            }
+        });
 
         dbHelper = new MyDBHelper(this);
         btnInit.setOnClickListener(new View.OnClickListener() {
@@ -36,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 db = dbHelper.getWritableDatabase();
                 dbHelper.onUpgrade(db, 1, 2);
+                selectDB();
                 db.close();
             }
         });
@@ -44,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 db = dbHelper.getWritableDatabase();
                 db.execSQL("insert into groupTB values('"+editName.getText().toString()+"', "+editCount.getText().toString()+");");
+                selectDB();
                 db.close();
                 Toast.makeText(getApplicationContext(), "정상적으로 행이 삽입 되었습니다.", Toast.LENGTH_SHORT).show();
             }
@@ -69,6 +93,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void selectDB(){
+        db = dbHelper.getReadableDatabase();
+        Cursor cursor = db.rawQuery("select * from groupTB;", null);
+        String strName = "그룹 이름\r\n_________\r\n";
+        String strCount = "인원\r\n_________\r\n";
+        while (cursor.moveToNext()){
+            strName += cursor.getString(0) + "\r\n";
+            strCount += cursor.getInt(1) + "\r\n";
+        }
+
+        editNameResult.setText(strName);
+        editCountResult.setText(strCount);
+
+        cursor.close();
+        db.close();
     }
 
     public class MyDBHelper extends SQLiteOpenHelper{
